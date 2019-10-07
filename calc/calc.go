f97c7a1e5acbffab86b458e1fc2f5eb1f11ca8b8
w84thesun <wait4thesun@gmail.com>
2019-10-07 13:29:46 +0300

    Update

--- calc/calc.go
+++ calc/calc.go
@@ -2,36 +2,19 @@ package calc
 
 import (
 	"strings"
-	"unicode"
-)
-
-const (
-	CHAR_OTHER  = 0
-	CHAR_LETTER = 1
 )
 
 func CalcRWords(txt, letter string) int {
-	num_r_words := 0
-	current_char_type := CHAR_OTHER
-	word := ""
-	for _, l := range txt {
-		if unicode.IsLetter(l) {
-			if current_char_type == CHAR_LETTER {
-				word += string(l)
-			} else {
-				current_char_type = CHAR_LETTER
-				word = string(l)
-			}
-		} else {
-			if current_char_type == CHAR_LETTER {
-				if strings.HasPrefix(strings.ToLower(word), strings.ToLower(letter)) {
-					num_r_words += 1
-				}
-			}
-			current_char_type = CHAR_OTHER
+	count := 0
+	words := strings.Fields(txt)
+
+	for _, word := range words {
+		word = strings.ToLower(strings.Trim(word, "\" \n'!.,-"))
+
+		if strings.HasPrefix(word, letter) {
+			count += 1
 		}
 	}
 
-	return num_r_words
+	return count
 }
-
