5c3777c095695c796c05e4e38ae6dc6f02ee0783
Alexey Andreev <bezumny.tochk@gmail.com>
2019-10-07 13:34:16 +0300

    fix

--- calc/calc.go
+++ calc/calc.go
@@ -1,37 +1,20 @@
 package calc
 
 import (
-	"strings"
 	"unicode"
 )
 
-const (
-	CHAR_OTHER  = 0
-	CHAR_LETTER = 1
-)
-
-func CalcRWords(txt, letter string) int {
-	num_r_words := 0
-	current_char_type := CHAR_OTHER
-	word := ""
-	for _, l := range txt {
-		if unicode.IsLetter(l) {
-			if current_char_type == CHAR_LETTER {
-				word += string(l)
-			} else {
-				current_char_type = CHAR_LETTER
-				word = string(l)
-			}
-		} else {
-			if current_char_type == CHAR_LETTER {
-				if strings.HasPrefix(strings.ToLower(word), strings.ToLower(letter)) {
-					num_r_words += 1
-				}
-			}
-			current_char_type = CHAR_OTHER
+func CalcRWords(txt string, letter rune) (numRWords int) {
+	f := true
+	for _, l := range []rune(txt) {
+		if !unicode.IsLetter(l) {
+			f = true
+			continue
 		}
+		if l == letter && f {
+			numRWords++
+		}
+		f = false
 	}
-
-	return num_r_words
+	return numRWords
 }
-
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ozonru/golangconf2019/calc"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
 /*
@@ -12,13 +13,13 @@ This script calculates the number of words in the text
 */
 
 const FILENAME = "Exler.txt"
-const LETTER = "р"
+const LETTER = rune('р')
 
 func main() {
 	txt, err := ioutil.ReadFile(FILENAME)
 	if err != nil {
 		log.Fatalf("Failed to open text file: %s", err.Error())
 	}
-	num := calc.CalcRWords(string(txt), LETTER)
-	fmt.Printf("The text %s contains %d words starting from letter '%s'\n", FILENAME, num, LETTER)
+	num := calc.CalcRWords(strings.ToLower(string(txt)), LETTER)
+	fmt.Printf("The text %s contains %d words starting from letter '%s'\n", FILENAME, num, string(LETTER))
 }
