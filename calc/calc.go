65f4cc8885844bca5abf0061f5c0d2521d96c832
Anton V <gto6ka@gmail.com>
2019-10-07 15:56:45 +0300

    simplifying and fixes

--- calc/calc.go
+++ calc/calc.go
@@ -12,26 +12,32 @@ const (
 
 func CalcRWords(txt, letter string) int {
 	num_r_words := 0
-	current_char_type := CHAR_OTHER
+	prev_char_type := CHAR_OTHER
 	word := ""
 	for _, l := range txt {
 		if unicode.IsLetter(l) {
-			if current_char_type == CHAR_LETTER {
-				word += string(l)
-			} else {
-				current_char_type = CHAR_LETTER
-				word = string(l)
+			word += string(l)
+			if prev_char_type != CHAR_LETTER {
+				prev_char_type = CHAR_LETTER
 			}
-		} else {
-			if current_char_type == CHAR_LETTER {
-				if strings.HasPrefix(strings.ToLower(word), strings.ToLower(letter)) {
-					num_r_words += 1
-				}
-			}
-			current_char_type = CHAR_OTHER
+			continue
 		}
+		if prev_char_type == CHAR_OTHER {
+			continue
+		}
+
+		// not letter and prev type - letter
+		if strings.HasPrefix(strings.ToLower(word), strings.ToLower(letter)) {
+			num_r_words += 1
+		}
+
+		prev_char_type = CHAR_OTHER
+		word = ""
+	}
+
+	if word != "" && strings.HasPrefix(strings.ToLower(word), strings.ToLower(letter)) {
+		num_r_words += 1
 	}
 
 	return num_r_words
 }
-
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -2,12 +2,13 @@ package calc
 
 import (
 	"testing"
+
 	"gotest.tools/assert"
-	)
+)
 
 func TestCalcRWords(t *testing.T) {
-	assert.Equal(t, CalcRWords("ааа ррр ббб", "р"), 1)
-	assert.Equal(t, CalcRWords("ааа ррр ббб\nробот\nработа твоя", "р"), 3)
+	assert.Equal(t, CalcRWords("ааа ррр ббб ррр", "р"), 2)
+	assert.Equal(t, CalcRWords("ааа ррр ббб\nробот\nработа твоя\nработа", "р"), 4)
 	assert.Equal(t, CalcRWords(`
 Главное, любой бы засмущался, находясь
 на вечеринке в штанах на пять размеров больше. А
